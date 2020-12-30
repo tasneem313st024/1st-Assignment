35f52e7209ef6ec711ae10de0f03bd090f82610a
Tasneem Abdul Raheem <75483083+tasneem313st024@users.noreply.github.com>
2020-12-29 23:02:03 -0800

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,10 @@
+# Addition,Subtraction,Multiplication,Division by taking input from user.
+a = int(input("Enter the 1st number="))
+b = int(input("Enter the 2nd number="))
+print("The Addition of", a, "and", b, "=", a+b)
+print("The Subtraction of", a, "and", b, "=", a-b)
+print("The Multiplication of", a, "and", b, "=", a*b)
+print("The Division of", a, "and", b, "=", a/b)
+
+
+
